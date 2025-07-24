Fix box cut gradient

src/net/e3gnn.py
@@ -375,8 +375,9 @@ class E3GNN(pl.LightningModule):
             create_graph=True,  # needed for second derivatives (eg. training on forces)
         )[0]
         return -grad_pos
-    
-    def get_stress(self,
+
+    def get_stress(
+        self,
         predictions: Dict[str, IrrepsBlockData],
         positions: torch.Tensor,
         box: torch.Tensor,
@@ -399,7 +400,9 @@ class E3GNN(pl.LightningModule):
             energy,
             box,
             create_graph=True,
-        )[0]         # (3,3)
+        )[
+            0
+        ]  # (3,3)
         # 2. compute volume
         volume = torch.det(box)
         # 3. form Cauchy stress: σ_{αβ} = (1/Ω) ∑_γ h_{γα} (dE/dh_{γβ})

tests/unit/test_stress.py
@@ -6,6 +6,7 @@ from net.e3gnn import E3GNN
 from net.common import HyperParams
 from data.factory import DatasetFactory
 
+
 def test_stress_with_real_data():
     """
     Tests that a randomly initialized network produces non-zero forces
@@ -54,4 +55,3 @@ def test_stress_with_real_data():
     assert not torch.allclose(
         stress, torch.zeros_like(stress)
     ), "Stress is all zero, something no worky."
- 