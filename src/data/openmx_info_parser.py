34c606164afe2bca453179c96f52d242097fa005
nerkulec <nerkulec@github.com>
2025-08-01 23:07:22 +0200

    Fixing double backward

--- src/data/openmx_info_parser.py
+++ src/data/openmx_info_parser.py
@@ -183,7 +183,7 @@ def parse_info_out(
             positions_list.append([float(v) for v in parts[2:5]])
             f_list.append([float(v) for v in parts[5:8]])
 
-    positions = torch.tensor(positions_list, dtype=dtype, requires_grad=True)
+    positions = torch.tensor(positions_list, dtype=dtype)
     forces = torch.tensor(f_list, dtype=dtype)
 
     # 5b) Fractional coords -------------------------------------------------
