52ea835311d5d3ceba41d53a9b7b541f87e60d85
nerkulec <nerkulec@github.com>
2025-08-02 00:02:33 +0200

    First big sweep

--- src/net/common.py
+++ src/net/common.py
@@ -131,7 +131,7 @@ class Config:
     gpus: int = 0  # number of GPUs
     num_workers: int = 0  # DataLoader workers, 0 for no parallelism
     save_dir: str = "checkpoints"  # directory to save model checkpoints
-    log_model: bool = True  # whether to log the model to WandB
+    log_model: bool = False  # whether to log the model to WandB
 
     # ----------------- caching ------------------------------------------
     cache_root: str | None = None  # path to cache directory, if any
