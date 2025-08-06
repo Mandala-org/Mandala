c18a15ade695352eaacce64d2f426ed70c0540fd
nerkulec <nerkulec@github.com>
2025-08-06 15:46:07 +0200

    New overfitting study run

--- studies/overfitting_study/run.sh
+++ studies/overfitting_study/run.sh
@@ -3,5 +3,12 @@
 # Activate the virtual environment
 source ../../mandala-venv/bin/activate
 
-# Run the study
-python3 run_study.py --num_samples 10 --num_epochs 200 --lr 0.001
+# Run the study with ReduceLROnPlateau scheduler and a new output folder
+python3 run_study.py \
+    --num_samples 10 \
+    --num_epochs 2000 \
+    --lr 0.001 \
+    --use_lr_scheduler \
+    --lr_scheduler_patience 5 \
+    --lr_scheduler_factor 0.5 \
+    --output_folder overfitting_with_scheduler
