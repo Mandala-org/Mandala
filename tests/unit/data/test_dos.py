8dc50fe38a2deebb1312f1c792e698d071fb6111
wiktoria-szopa <szopa.wiktoria1@gmail.com>
2025-08-11 16:35:12 +0200

    Added .dos() test to check number of states

--- tests/unit/data/test_dos.py
+++ tests/unit/data/test_dos.py
@@ -22,8 +22,30 @@ def test_dos_return_shape():
     bin_width = 0.001
     E_min = -1.0
     E_max = 1.0
+
     dos = snap.dos(sigma, bin_width, E_min, E_max)
     expected_length = int((E_max - E_min) / bin_width) + 1
 
     assert np.shape(dos["energies"]) == (expected_length,)
     assert np.shape(dos["dos"]) == (expected_length,)
+
+
+def test_number_states():
+    snap = _load_snapshot()
+
+    sigma = 0.005
+    bin_width = 0.001
+    E_min = -100
+    E_max = 100
+
+    dos = snap.dos(sigma, bin_width, E_min, E_max)
+    E_grid = dos["energies"]
+    dos = dos["dos"]
+    H = snap.hamiltonian.to_dense().detach().numpy()
+    
+    expected_n_states = np.shape(H)[0]
+    dos_n_states = int(np.trapezoid(dos, E_grid))
+    assert 1 - dos_n_states/ expected_n_states < 0.01
+    print('es')
+
+test_number_states()
