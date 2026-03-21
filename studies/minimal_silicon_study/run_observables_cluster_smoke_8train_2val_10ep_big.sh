5491edae5ba74e55dfdb7839be67e614f223f7f7
nerkulec <nerkulec@github.com>
2026-03-21 20:56:01 +0100

    Smaller run

--- studies/minimal_silicon_study/run_observables_cluster_smoke_8train_2val_10ep_big.sh
+++ studies/minimal_silicon_study/run_observables_cluster_smoke_8train_2val_10ep_big.sh
@@ -30,7 +30,7 @@ python studies/minimal_silicon_study/train_silicon_minimal.py \
   --train-on-forces true \
   --loss-coef-forces 1e-8 \
   --symmetrize-preds true \
-  --hidden-irreps 48x0e+32x1e+32x1o+24x2e+24x2o+16x3e+16x3o+8x4e \
+  --hidden-irreps 32x0e+24x1e+24x1o+16x2e+16x2o+12x3e+12x3o+8x4e \
   --hidden-dim 32 \
   --l-max 4 \
   --num-layers 2 \
