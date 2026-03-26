377bcb777bf00bd291c2220cb19505d532ce76c2
nerkulec <nerkulec@github.com>
2026-03-26 18:01:41 +0100

    Smaller irreps

--- studies/minimal_silicon_study/run_restart_debug_10train_1val_100_plus_100.sh
+++ studies/minimal_silicon_study/run_restart_debug_10train_1val_100_plus_100.sh
@@ -40,7 +40,7 @@ COMMON_ARGS=(
   --loss-coef-density-matrix 1
   --loss-coef-forces 1e-8
   --symmetrize-preds true
-  --hidden-irreps 32x0e+32x1e+32x1o+16x2e+16x2o+16x3e+16x3o+8x4e
+  --hidden-irreps 24x0e+16x1e+16x1o+8x2e+8x2o+8x3e+8x3o+4x4e
   --hidden-dim 32
   --l-max 4
   --num-layers 2
