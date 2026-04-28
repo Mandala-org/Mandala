4e92b80524323956d8df4ed271b11a40bd8bfd87
nerkulec <nerkulec@github.com>
2026-04-28 10:50:12 +0200

    Cleanup

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,5 +3,11 @@ utils package
 """
 
 from .summary import print_model_summary, print_dataset_summary
+from .run_name import resolve_run_name, sanitize_run_name
 
-__all__ = ["print_model_summary", "print_dataset_summary"]
+__all__ = [
+    "print_model_summary",
+    "print_dataset_summary",
+    "resolve_run_name",
+    "sanitize_run_name",
+]
